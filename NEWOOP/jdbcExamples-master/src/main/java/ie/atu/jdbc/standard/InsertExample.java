64eba18c50799c942e1dbd20bc3a56699c09884b
Nathan <G00413711@atu.ie>
2024-03-11 16:46:13 +0000

    Created insert into my table customer

--- NEWOOP/jdbcExamples-master/src/main/java/ie/atu/jdbc/standard/InsertExample.java
+++ NEWOOP/jdbcExamples-master/src/main/java/ie/atu/jdbc/standard/InsertExample.java
@@ -12,16 +12,16 @@ public class InsertExample {
         try {
 
             // Insert a new record into the "users" table
-            PreparedStatement stmt = conn.prepareStatement("INSERT INTO users (username, password) VALUES (?, ?)");
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO customer (name, email) VALUES (?, ?)");
             stmt.setString(1, "Des");
-            stmt.setString(2, "software");
+            stmt.setString(2, "@desemail");
             stmt.executeUpdate();
 
             // Insert a new record into the "emails" table, referencing the new user
-            stmt = conn.prepareStatement("INSERT INTO emails (user_id, email) VALUES (?, ?)");
-            stmt.setInt(1, getLastInsertId(conn));
-            stmt.setString(2, "des@atu.ie");
-            stmt.executeUpdate();
+            //stmt = conn.prepareStatement("INSERT INTO emails (user_id, email) VALUES (?, ?)");
+            //stmt.setInt(1, getLastInsertId(conn));
+            //stmt.setString(2, "des@atu.ie");
+            //stmt.executeUpdate();
 
             System.out.println("Insert completed successfully.");
         } catch (SQLException ex) {
